101d743ffd00cf58758c5c105d4fd13c85f26dd4
Pelle Wessman <pelle@kodfabrik.se>
2023-07-20 18:02:28 +0200

    Export `PhrasingContentOrStringList`

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,10 @@
 export type * from './lib/advanced-types.d.ts';
 
+export type {
+  PhrasingContentOrString,
+  PhrasingContentOrStringList,
+} from './lib/mdast-helpers.js';
+
 export { mdastListHelper } from './lib/list.js';
 export { MarkdownOrChalk } from './lib/main.js';
 export { mdastLinkify } from './lib/mdast-helpers.js';
